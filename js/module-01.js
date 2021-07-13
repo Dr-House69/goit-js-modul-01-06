c520c44ba5b694cedcc9d477ed4416d1f7c5fb82
Dr-House69 <ihor.dombik@gmail.com>
2021-07-14 02:17:12 +0300

    add tasks-01-14
    add tasks-01-14

---
+++ js/module-01.js
@@ -0,0 +1,105 @@
+/* task-05 */
+
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`
+
+// console.log(message);
+
+/* task-06 */
+
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+// console.log(message);
+
+/*  task-07 */
+
+// function sayHi() {
+//     console.log("Hello, this is my first function!");
+
+// }
+// sayHi();
+
+/* task-08 */
+
+// function add(a, b, c) {
+//     console.log(`Addition result equals ${a+b+c}`);
+// }
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+/* task-09 */
+
+// function add(a, b, c) {
+   
+//     return a + b + c;
+
+// }
+
+//     add(2, 5, 8); 
+
+//     console.log(add(15, 27, 10));
+//     console.log(add(10, 20, 30));
+//     console.log(add(5, 10, 15));
+
+/*  task-10 */
+
+// function makeMessage (name, price) {
+  
+// const message = `You picked ${name}, price per item is ${price} credits`;
+ 
+//  return message;
+// }
+// console.log(makeMessage('Radar', 6150));
+
+/*  task-11 */
+
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
+  
+//     const totalPrice = orderedQuantity * pricePerItem;
+
+  
+//   return totalPrice;
+// };
+// console.log(calculateTotalPrice(3, 400));
+
+/* task-12 */
+
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+  
+//     const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+//     const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+
+ 
+//   return message;
+// }
+// console.log(makeOrderMessage(2, 100, 50));
+
+/* task-13 */
+
+// function isAdult(age) {
+  
+//   const passed = age >= 18;
+
+//   return passed;
+// }
+// console.log(isAdult(51));
+
+/* task-14 */
+
+function isValidPassword(password) {
+  const SAVED_PASSWORD = 'jqueryismyjam';
+ 
+  const isMatch = password === 'jqueryismyjam';
+
+   return isMatch;
+}
+console.log(isValidPassword('mangodab3st'));
+
+/* task-15 */
