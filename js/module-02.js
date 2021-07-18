9e2690f152bc98cb367c630a5c062dea40d51ee5
Dr-House69 <ihor.dombik@gmail.com>
2021-07-18 22:00:25 +0300

    Add tasks-01-10 modulle-2
    Add tasks-01-10 modulle-2

--- js/module-02.js
+++ js/module-02.js
@@ -12,8 +12,8 @@
 
 // function checkPassword(password) {
 //   const ADMIN_PASSWORD = 'jqueryismyjam';
-  
-  
+
+
 //   if (password === ADMIN_PASSWORD) {
 //     return 'Welcome!';
 //   } 
@@ -24,18 +24,99 @@
 /* task-03 */
 
 // function checkStorage(available, ordered) {
-  
+
 //     if (ordered === 0) {
 //         return 'Your order is empty!';
 //     }
 //     if (ordered > available) {
 //         return 'Your order is too large, not enough goods in stock!';
 //     }
-  
+
 //         return 'The order is accepted, our manager will contact you';
- 
+
 
 // }
 // console.log( checkStorage(10,0));
 
 /* task-04 */
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+
+/* task-05 */
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits[fruits.length - 1];
+// console.log(fruits);
+
+
+/* task-06 */
+
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+
+// fruits[1] = "peach";
+// fruits[3] = "banana";
+// console.log(fruits[1]);
+
+/* task-07 */
+
+
+// const fruits = ["apple", "peach", "pear", "banana"];
+
+// const fruitsArrayLength = fruits.length;
+// console.log(fruitsArrayLength);
+
+
+/* task-08 */
+
+// const fruits = ["apple", "peach", "pear", "banana"];
+
+
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex];
+// console.log(lastElement);
+
+
+/* task-09 */
+
+// function getExtremeElements(array) {
+
+//     // const firstElement = array[0];
+//     // const lastElement = array[array.length - 1];
+//     array = [array[0], array[array.length - 1]];
+
+//     return  array;
+  
+
+// }
+// console.log(getExtremeElements(['apple', 'peach', 'pear', 'banana']));
+
+
+/* task-10 */
+
+// function splitMessage(message, delimeter) {
+//   let words;
+
+//     // delimeter = '',' ','_';
+//     words = message.split(delimeter);
+    
+//   return words;
+// }
+// console.log(splitMessage('Mango_hurries_to_the_train','_'));
+
+
+/* task-11 */
+
+function calculateEngravingPrice(message, pricePerWord) {
+   // Change code below this line
+
+
+
+   // Change code above this line
+}
